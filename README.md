4f7aacd77aedbbf6516dd92c5fe25ec6a0c32293
Dhanu1402 <dhanukumar2000@gmail.com>
2023-02-22 15:30:40 +0530

    Installing vite with react & setting up tailwind

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chat App
+
+# Learnings
+
+1. Installing vite through react and setting up tailwind css
---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="bg-red-500">test</div>;
+}
+
+export default App;
